2067e336bef0c0a55ea07422f336c57b237ad7b4
Pasala Sandhya <131899418+sandhya28101998@users.noreply.github.com>
2023-11-29 12:46:42 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,82 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Herosection from "./components/Hero-section";
-//import Cardsection from "./components/Card";
 import Section from "./components/Section";
+import SectionAllSongs from "./components/SectionAllsongs";
+import Accordions from "./components/Accordion";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 const ENDPOINT = "https://qtify-backend-labs.crio.do/";
+
 function App() {
-  const [topAlbums, SetTopAlbums] = useState([]);
-  const [NewAlbums, SetNewAlbums] = useState([]);
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [newAlbums, setNewAlbums] = useState([]);
+  const [allSongs, setAllSongs] = useState([]);
+  const [generData, setGenerData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
 
   useEffect(() => {
-    axios.get(`${ENDPOINT}albums/top`).then(({ data }) => {
-      SetTopAlbums(data);
-    });
-    axios.get(`${ENDPOINT}albums/new`).then(({ data }) => {
-      SetNewAlbums(data);
-    });
+    const playList = async () => {
+      try {
+        const restop = await axios.get(`${ENDPOINT}albums/top`);
+        if (restop.status === 200) {
+          setTopAlbums(restop.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      try {
+        const resnew = await axios.get(`${ENDPOINT}albums/new`);
+        if (resnew.status === 200) {
+          setNewAlbums(resnew.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      try {
+        const ressong = await axios.get(`${ENDPOINT}songs`);
+        if (ressong.status === 200) {
+          setAllSongs(ressong.data);
+          setFilterData(ressong.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      try {
+        const resgener = await axios.get(`${ENDPOINT}genres`);
+        if (resgener.status === 200) {
+          setGenerData([{ key: "all", label: "All" }, ...resgener.data.data]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    playList();
   }, []);
+
   return (
     <div>
       <Navbar />
       <Herosection />
-      <Section title={"Top Albums"} data={topAlbums} />
-      <Section title={"New Albums"} data={NewAlbums} />
+      <Section navId="ta" title={"Top Albums"} data={topAlbums} />
+      <Section navId="na" title={"New Albums"} data={newAlbums} />
+      <SectionAllSongs
+        title={"Songs"}
+        songs={filterData}
+        resgener={generData}
+        excuteFilter={(genre) => {
+          if (genre === "all") {
+            setFilterData(allSongs);
+          } else {
+            setFilterData(allSongs.filter((song) => genre === song.genre.key));
+          }
+        }}
+      />
+      <Accordions title={"FAQs"} />
     </div>
   );
 }
